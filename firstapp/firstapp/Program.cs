f8ae0ce93585afe7eb7b020a21a0529eb656331b
ahmed <ahmed@DESKTOP-6CTEJHE>
2018-11-11 10:54:13 +0200

    my first
    this is the first pro

---
+++ firstapp/firstapp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is my first program
+            int myValue;
+            myValue = 10;
+            Console.WriteLine("Hello World"+myValue);
+            Console.ReadKey();
+        }
+    }
+}
