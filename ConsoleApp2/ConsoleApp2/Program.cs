730aac81e16f5c93cf4dcb7ab54fd8850df83767
ahmed <ahmed@DESKTOP-6CTEJHE>
2018-11-11 10:59:08 +0200

    the second
    the upload the second pro on the github

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myValue;
+            myValue = 7;
+            Console.WriteLine("I'm nember :- "+myValue);
+            Console.ReadKey();
+
+        }
+    }
+}
